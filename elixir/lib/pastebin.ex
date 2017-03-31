945d68cdafbba5396bf9470c12375e57f97e0142
Max Holder <mxhold@gmail.com>
2017-03-31 16:30:12 -0700

    Simplify response url for Elixir

--- elixir/lib/pastebin.ex
+++ elixir/lib/pastebin.ex
@@ -43,12 +43,9 @@ defmodule Pastebin do
       sql = "INSERT INTO posts (id, body) VALUES ($1, $2)"
       Sqlitex.Server.query(Sqlitex.Server, sql, bind: [post_id, body])
 
-      port_part = if conn.port == 80 do "" else ":#{conn.port}" end
-      base_url = "#{conn.scheme}://#{conn.host}#{port_part}"
-
       conn
       |> put_resp_content_type("text/plain")
-      |> send_resp(201, "#{base_url}/#{post_id}")
+      |> send_resp(201, "#{conn.scheme}://#{get_req_header(conn, "host")}/#{post_id}")
     end
 
     match "/:post_id" do
