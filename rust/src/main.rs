261349ab9ee938426398997996e022e55a7564f1
Max Holder <mxhold@gmail.com>
2017-03-31 13:31:07 -0700

    Inline Rust queries

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,8 +7,9 @@ extern crate rusqlite;
 extern crate uuid;
 
 use iron::prelude::*;
-use iron::{headers, status};
+use iron::status;
 use iron::modifiers::Header;
+use iron::headers::ContentType;
 use std::io::Read;
 use std::env;
 use std::process;
@@ -18,54 +19,32 @@ impl iron::typemap::Key for ConnectionPool {
     type Value = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
 }
 
-struct DatabaseConnection {
-    conn: r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager>,
-}
-
-impl DatabaseConnection {
-    fn new(pool: &r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>) -> DatabaseConnection {
-        DatabaseConnection { conn: pool.get().unwrap() }
-    }
-
-    fn setup_database(&self) -> Result<i32, rusqlite::Error> {
-        let query = "CREATE TABLE IF NOT EXISTS posts (id TEXT, body BLOB)";
-        self.conn.execute(query, &[])
-    }
-
-    fn insert_paste(&self, body: &str) -> Result<String, rusqlite::Error> {
-        let id = uuid::Uuid::new_v4().to_string();
-        let query = "INSERT INTO posts VALUES ($1, $2)";
-        self.conn.execute(query, &[&id, &body]).and(Ok(id))
-    }
-
-    fn get_paste_body_by_id(&self, id: &str) -> Option<String> {
-        let query = "SELECT body FROM posts WHERE id = $1";
-        self.conn.query_row(query, &[&id], |row| row.get(0)).ok()
-    }
-}
-
 fn post_pastebin(req: &mut Request) -> IronResult<Response> {
     let pool = req.get::<persistent::Read<ConnectionPool>>().unwrap();
-    let conn = DatabaseConnection::new(&pool);
+    let conn = pool.get().unwrap();
 
     let mut req_body = String::new();
     req.body.read_to_string(&mut req_body).unwrap();
 
-    match conn.insert_paste(&req_body) {
-        Ok(id) => Ok(Response::with((status::Created, Header(headers::ContentType::plaintext()), id))),
-        Err(_) => Ok(Response::with((status::ServiceUnavailable, Header(headers::ContentType::plaintext()), ""))),
+    let id = uuid::Uuid::new_v4().to_string();
+    let sql = "INSERT INTO posts VALUES ($1, $2)";
+
+    match conn.execute(sql, &[&id, &req_body]) {
+        Ok(_) => Ok(Response::with((status::Created, Header(ContentType::plaintext()), id))),
+        Err(_) => Ok(Response::with((status::ServiceUnavailable, Header(ContentType::plaintext()), ""))),
     }
 }
 
 fn get_pastebin(req: &mut Request) -> IronResult<Response> {
     let pool = req.get::<persistent::Read<ConnectionPool>>().unwrap();
-    let conn = DatabaseConnection::new(&pool);
+    let conn = pool.get().unwrap();
 
     let id = req.extensions.get::<router::Router>().unwrap().find("id").unwrap();
 
-    match conn.get_paste_body_by_id(&id) {
-        Some(body) => Ok(Response::with((status::Ok, Header(headers::ContentType::plaintext()), body))),
-        None => Ok(Response::with((status::NotFound, Header(headers::ContentType::plaintext()), "Not found\n"))),
+    let sql = "SELECT body FROM posts WHERE id = $1";
+    match conn.query_row(sql, &[&id], |row| -> String { row.get(0) }) {
+        Ok(body) => Ok(Response::with((status::Ok, Header(ContentType::plaintext()), body))),
+        Err(_) => Ok(Response::with((status::NotFound, Header(ContentType::plaintext()), "Not found\n"))),
     }
 }
 
@@ -77,7 +56,9 @@ fn main() {
     let config = r2d2::Config::default();
     let manager = r2d2_sqlite::SqliteConnectionManager::new("./db.sqlite3");
     let pool = r2d2::Pool::new(config, manager).unwrap();
-    DatabaseConnection::new(&pool).setup_database().unwrap();
+    let conn = pool.get().unwrap();
+    let sql = "CREATE TABLE IF NOT EXISTS posts (id TEXT, body BLOB)";
+    conn.execute(sql, &[]).unwrap();
 
     let mut middleware = Chain::new(router);
     middleware.link_before(persistent::Read::<ConnectionPool>::one(pool));
